ea28353f49b77e715cf3d80f7d689bd827d26dd8
remisimaer <20785833+remisimaer@users.noreply.github.com>
2025-12-10 23:35:48 +0100

    external links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,18 @@ menuTrigger.onclick = function() {
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
 }
+
+// Open all external links in new tab
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a[href^="http"]');
+    var host = window.location.hostname;
+    
+    links.forEach(function(link) {
+        var linkHost = link.hostname;
+        // Check if link is external
+        if (linkHost !== host && linkHost !== '') {
+            link.setAttribute('target', '_blank');
+            link.setAttribute('rel', 'noopener noreferrer');
+        }
+    });
+});
